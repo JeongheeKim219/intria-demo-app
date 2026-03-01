9565d096641b7f16f03c3448677a6731dd3b2e56
Jeonghee <jeongheekim219@gmail.com>
2026-03-01 18:47:47 +0900

    feat: PostgreSQL 도커 환경 및 DB 스키마 초기화 스크립트 추가

---
+++ app/database/db.py
@@ -0,0 +1,11 @@
+# db.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
